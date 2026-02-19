a0d60fc498d4a65c64eced1330abbba1158b1e81
Pushkar06p <pushkar06pandey@gmail.com>
2026-02-19 14:11:52 +0530

    added warehouse app in urls

--- backend/python/django_app/urls.py
+++ backend/python/django_app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.http import JsonResponse
-from django_app.adapters.api.views import hello_view
+from django_app.adapters.api.views import hello_view 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_view),
+    path ('warehouse/', include("warehouse.urls"))
 ]
